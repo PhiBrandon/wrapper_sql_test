cbeb5e3c8fd0c4db1e6549b5b7c838d7be0a532b
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-03-28 10:09:08 -0600

    Refactored code and removed database information to make it a little more reusable

--- lang_sql.py
+++ lang_sql.py
@@ -1,7 +1,5 @@
+import os
 from dotenv import load_dotenv
-
-load_dotenv()
-
 from langchain_community.utilities import SQLDatabase
 from langchain_anthropic import ChatAnthropic
 from langchain.chains import create_sql_query_chain
@@ -9,13 +7,7 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel
 
-db = SQLDatabase.from_uri("postgresql://postgres:postgres@localhost:5432/deez")
-print(db.dialect)
-print(db.get_table_info())
-print(db.get_usable_table_names())
-db.run("select * from jobdata limit 10")
-
-model = ChatAnthropic(model="claude-3-haiku-20240307")
+load_dotenv()
 
 
 class SQLResponse(BaseModel):
@@ -23,39 +15,53 @@ class SQLResponse(BaseModel):
     sql_query: str
 
 
-prompt = """
-You are a PostgreSQL expert. Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer to the input question.\nUnless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per PostgreSQL. You can order the results to return the most informative data in the database.\nNever query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.\nPay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.\nPay attention to use CURRENT_DATE function to get the current date, if the question involves "today".Pay attention to generate queries that would be the most efficient based on the user question and the columns provided.\n\nHere's a JSON schema to follow: {pydantic_format} Output a valid JSON object but do not repeat the schema. . Only use the following tables:\n{table_info}\n\nQuestion: {input}. Only output the valid JSON response. Do not include any other text or explanation in your response. If the questions contains arrays, make sure the final output that contains it does not have them.
-"""
-aprompt = PromptTemplate(
-    input_variables=["input", "table_info", "pydantic_format", "top_k"],
-    template=prompt,
-)
+def main():
+    db = SQLDatabase.from_uri("postgresql://username:password@localhost:5432/database_name")
+
+    # Initialize the model
+    model = ChatAnthropic(model="claude-3-haiku-20240307")
+
+    prompt = """
+    You are a PostgreSQL expert. Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer to the input question.\\nUnless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per PostgreSQL. You can order the results to return the most informative data in the database.\\nNever query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.\\nPay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.\\nPay attention to use CURRENT_DATE function to get the current date, if the question involves "today".Pay attention to generate queries that would be the most efficient based on the user question and the columns provided.\\n\\nHere's a JSON schema to follow: {pydantic_format} Output a valid JSON object but do not repeat the schema. . Only use the following tables:\\n{table_info}\\n\\nQuestion: {input}. Only output the valid JSON response. Do not include any other text or explanation in your response. If the questions contains arrays, make sure the final output that contains it does not have them.
+    """
+
+    custom_sql_prompt = PromptTemplate(
+        input_variables=["input", "table_info", "pydantic_format", "top_k"],
+        template=prompt,
+    )
+
+    chain = create_sql_query_chain(model, db, prompt=custom_sql_prompt) | JsonOutputParser()
+
+    response = chain.invoke(
+        {
+            "question": "Which job descriptions contain AWS? give the IDs",
+            "pydantic_format": SQLResponse.model_json_schema(),
+            "top_k": 5,
+        }
+    )
+    print(response)
+
+    valid_query = SQLResponse.model_validate(response)
+    print(valid_query.sql_query)
+
+    query_output_1 = db.run(valid_query.sql_query)
+    print(query_output_1)
+
+    response_followup = chain.invoke(
+        {
+            "question": f"What are the job titles for these descriptions: {str(query_output_1)}",
+            "pydantic_format": SQLResponse.model_json_schema(),
+            "top_k": 5,
+        }
+    )
+    print(response_followup)
+
+    valid_query_2 = SQLResponse.model_validate(response_followup)
+    print(valid_query_2.sql_query)
+
+    query_output_2 = db.run(valid_query_2.sql_query)
+    print(query_output_2)
 
 
-chain = create_sql_query_chain(model, db, prompt=aprompt) | JsonOutputParser()
-response = chain.invoke(
-    {
-        "question": "Which job descriptions contain AWS? give the IDs",
-        "pydantic_format": SQLResponse.model_json_schema(),
-        "top_k": 5,
-    }
-)
-print(response)
-valid_query = SQLResponse.model_validate(response)
-print(valid_query.sql_query)
-out_1 = db.run(valid_query.sql_query)
-print(out_1)
-# [('27da1212b273cb30',), ('c58943135c8b3851',), ('c95b7576c1d1bf76',), ('ab6ba103dae1c0db',), ('c82a9ec04eb1d554',)]
-
-
-response_followup = chain.invoke(
-    {
-        "question": f"What are the job titles for these descriptions: {str(out_1)}",
-        "pydantic_format": SQLResponse.model_json_schema(),
-        "top_k": 5,
-    }
-)
-print(response_followup)
-valid_query_2 = SQLResponse.model_validate(response_followup)
-print(valid_query_2.sql_query)
-db.run(valid_query_2.sql_query)
+if __name__ == "__main__":
+    main()
